b9b7648f51593e60c859fc0776ce54841b2990d1
kano gooop <kanobucuo@gmail.com>
2019-09-21 17:59:28 +0800

    09-21

--- pages/chuang-publish/chuang-publish.js
+++ pages/chuang-publish/chuang-publish.js
@@ -2,24 +2,59 @@ const app = getApp();
 
 Page({
   data: {
-    textarea_padding: '15rpx',
+    is_ios: false,
+
+    tag_list: [],
+
     req_id: 0,
     title: '',  // 创意标题
     content: '',  // 创意内容
     content_count: 0
   },
   onLoad(options) {
-    let phone = wx.getSystemInfoSync();
-    if (phone.platform === 'ios') {
-      this.setData({ textarea_padding: '0rpx 5rpx' })
-    }
-
+    this.setData({ is_ios: app.is_ios });
     this.data.req_id = options.req_id;
+    this.ideaTagsList();
+  },
+  // 创意标签列表
+  ideaTagsList() {
+    app.ajax('api/ideaTagsList', null, res => {
+      for (let i = 0; i < res.length; i++) {
+        res[i].flex_pad = app.null_arr(res[i].child.length, 5);
+        for (let j = 0; j < res[i].child; j++) {
+          res[i].child[j].selected = false;
+        }
+      }
+      this.setData({ tag_list: res });
+    });
+  },
+  // 选择标签
+  tag_choose(e) {
+    let index = e.currentTarget.dataset.index;
+    let sub_index = e.currentTarget.dataset.sub_index;
+
+    if (this.get_tags().length >= 5 && !this.data.tag_list[index].child[sub_index].selected) {
+      app.toast('标签最多选择5个');
+    } else {
+      this.setData({ [`tag_list[${index}].child[${sub_index}].selected`]: !this.data.tag_list[index].child[sub_index].selected });
+    }
+  },
+  // 获得选中标签id列表
+  get_tags() {
+    let tags = [];
+    for (let i = 0; i < this.data.tag_list.length; i++) {
+      for (let j = 0; j < this.data.tag_list[i].child.length; j++) {
+        if (this.data.tag_list[i].child[j].selected) {
+          tags.push(this.data.tag_list[i].child[j].id);
+        }
+      }
+    }
+    return tags;
   },
   bind_input(e) {
     app.bind_input(e, this);
     if (e.currentTarget.dataset.name === 'content') {
-      this.setData({content_count: this.data.content.length});
+      this.setData({ content_count: this.data.content.length });
     }
   },
   // 提出创意
@@ -33,7 +68,8 @@ Page({
       let post = {
         req_id: data.req_id,
         title: data.title,
-        content: data.content
+        content: data.content,
+        tags: this.get_tags().toString()
       };
 
       wx.showLoading({ mask: true });
--- pages/person-list/person-list.js
+++ pages/person-list/person-list.js
@@ -3,12 +3,34 @@ const app = getApp();
 Page({
   data: {
     type: 1,
-    nomore: false,
-    nodata: true
+
+    loading: false,
+
+    // 博物馆
+    museum_list: [],
+    museum_page: 1,
+    museum_nomore: false,
+    museum_nodata: false,
+
+    // 设计师
+    designer_list: [],
+    designer_page: 1,
+    designer_nomore: false,
+    designer_nodata: false,
+
+    // 工厂
+    factory_list: [],
+    factory_page: 1,
+    factory_nomore: false,
+    factory_nodata: false
   },
   onLoad(options) {
-    this.data.type = parseInt(options.type);
+    this.setData({ type: parseInt(options.type) });
     this.set_title(this.data.type);
+
+    this.bwgList();
+    this.designerList();
+    this.factoryList();
   },
   // 切换tab
   tab_change(e) {
@@ -29,5 +51,224 @@ Page({
         wx.setNavigationBarTitle({ title: '工厂列表' });
         break;
     }
+  },
+  // 博物馆列表
+  bwgList(complete) {
+    let post = {
+      page: this.data.museum_page,
+      perpage: 10
+    };
+
+    app.ajax('api/bwgList', post, res => {
+      if (res.length === 0) {
+        if (this.data.museum_page === 1) {
+          this.setData({
+            museum_list: [],
+            museum_nodata: true,
+            museum_nomore: false
+          });
+        } else {
+          this.setData({
+            museum_nomore: true,
+            museum_nodata: false
+          });
+        }
+      } else {
+        app.format_img(res, 'cover');
+
+        this.setData({ museum_list: this.data.museum_list.concat(res) });
+      }
+      this.data.museum_page++;
+    }, null, () => {
+      if (complete) {
+        complete()
+      }
+    });
+  },
+  // 设计师列表
+  designerList(complete) {
+    let post = {
+      page: this.data.designer_page,
+      perpage: 20
+    };
+
+    app.ajax('api/designerList', post, res => {
+      if (res.length === 0) {
+        if (this.data.designer_page === 1) {
+          this.setData({
+            designer_list: [],
+            designer_nodata: true,
+            designer_nomore: false
+          });
+        } else {
+          this.setData({
+            designer_nomore: true,
+            designer_nodata: false
+          });
+        }
+      } else {
+        app.avatar_format(res, 'avatar');
+
+        this.setData({ designer_list: this.data.designer_list.concat(res) });
+      }
+      this.data.designer_page++;
+    }, null, () => {
+      if (complete) {
+        complete()
+      }
+    });
+  },
+  // 工厂列表
+  factoryList(complete) {
+    let post = {
+      page: this.data.factory_page,
+      perpage: 10
+    };
+
+    app.ajax('api/factoryList', post, res => {
+      if (res.length === 0) {
+        if (this.data.factory_page === 1) {
+          this.setData({
+            factory_list: [],
+            factory_nodata: true,
+            factory_nomore: false
+          });
+        } else {
+          this.setData({
+            factory_nomore: true,
+            factory_nodata: false
+          });
+        }
+      } else {
+        app.format_img(res, 'cover');
+
+        this.setData({ factory_list: this.data.factory_list.concat(res) });
+      }
+      this.data.factory_page++;
+    }, null, () => {
+      if (complete) {
+        complete()
+      }
+    });
+  },
+  // 下拉刷新
+  onPullDownRefresh() {
+    if (!this.data.loading) {
+      this.data.loading = true;
+
+      // 重置列表数据
+      this.reset(this.data.type);
+
+      wx.showNavigationBarLoading();
+      switch (this.data.type) {
+        case 1:
+          this.bwgList(() => {
+            this._refresh_after();
+          });
+          break;
+        case 2:
+          this.designerList(() => {
+            this._refresh_after();
+          });
+          break;
+        case 3:
+          this.factoryList(() => {
+            this._refresh_after();
+          });
+          break;
+      }
+    }
+  },
+  // 下拉刷新后
+  _refresh_after() {
+    this.data.loading = false;
+    wx.hideNavigationBarLoading();
+    wx.stopPullDownRefresh();
+  },
+  // 上拉加载
+  onReachBottom() {
+    if (!this.data.nomore && !this.data.nodata) {
+      if (!this.data.loading) {
+        this.data.loading = true;
+        wx.showNavigationBarLoading();
+
+        switch (this.data.type) {
+          case 1:
+            this.bwgList(() => {
+              this._reach_after();
+            });
+            break;
+          case 2:
+            this.designerList(() => {
+              this._reach_after();
+            });
+            break;
+          case 3:
+            this.factoryList(() => {
+              this._reach_after();
+            });
+            break;
+        }
+      }
+    }
+  },
+  // 上拉加载后
+  _reach_after() {
+    wx.hideNavigationBarLoading();
+    this.data.loading = false;
+  },
+  // 重置（博物馆/设计师/工厂）
+  reset(type) {
+    switch (type) {
+      // 博物馆
+      case 1:
+        this.data.museum_page = 1;
+        this.data.museum_list = [];
+        this.setData({
+          museum_nomore: false,
+          museum_nodata: false
+        });
+        break;
+      // 设计师
+      case 2:
+        this.data.designer_page = 1;
+        this.data.designer_list = [];
+        this.setData({
+          designer_nomore: false,
+          designer_nodata: false
+        });
+        break;
+      // 工厂
+      case 3:
+        this.data.factory_page = 1;
+        this.data.factory_list = [];
+        this.setData({
+          factory_nomore: false,
+          factory_nodata: false
+        });
+        break;
+    }
+  },
+  // 关注/取消关注
+  iFocus(e) {
+    if (!this.data.loading) {
+      this.data.loading = true;
+
+      let index = e.currentTarget.dataset.index;
+      let designer = this.data.designer_list[index];
+      app.ajax('note/iFocus', { to_uid: designer.id }, res => {
+        designer.if_focus = res;
+        res ? designer.focus++ : designer.focus--;
+        this.setData({ [`designer_list[${index}]`]: designer });
+      }, null, () => {
+        this.data.loading = false;
+      });
+    }
+  },
+  // 去他人主页
+  to_person_page(e) {
+    app.page_open(() => {
+      wx.navigateTo({ url: '/pages/person-page/person-page?uid=' + e.currentTarget.dataset.id });
+    });
   }
 });
