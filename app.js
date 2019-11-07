0d90073b8631d5597d0e863bd6af2f66cb576695
kano gooop <kanobucuo@gmail.com>
2019-11-07 18:19:37 +0800

    11-07

--- app.js
+++ app.js
@@ -565,5 +565,34 @@ App({
   // 格式化数字，如果没有小数则返回整数，有小数返回小数
   num_zheng(num) {
     return parseInt(num) === parseFloat(num) ? parseInt(num) : parseFloat(num);
+  },
+  // 多久以前的友好式格式化
+  ago_text(timestamp) {
+    let now_ts = parseInt(new Date().getTime() / 1000);
+    let diff = now_ts - timestamp;
+    if (diff <= 2592000) {
+      // 小于30天
+      if (diff < 0) {
+        return '刚刚发表';
+      } else if (diff < 60) {
+        return diff + '秒前';
+      } else if (diff < 3600) {
+        return parseInt(diff / 60) + '分前';
+      } else if (diff < 86400) {
+        return parseInt(diff / 3600) + '小时前';
+      } else {
+        return parseInt(diff / 86400) + '天前';
+      }
+    } else {
+      // 大于30天
+      if (new Date(timestamp * 1000).getFullYear() === new Date(now_ts * 1000).getFullYear()) {
+        // 在今年之内
+        return utils.date_format(timestamp, 'MM-dd hh:mm');
+      } else {
+        // 不在今年之内
+        console.log(timestamp);
+        return utils.date_format(timestamp, 'yyyy-MM-dd hh:mm');
+      }
+    }
   }
 });
