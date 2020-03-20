7548b303228f9d87ed06dfd273f8dbc322c72f64
kano gooop <kanobucuo@gmail.com>
2020-03-20 17:59:24 +0800

    2020-03-20

--- packageNew/pages/index/index.js
+++ packageNew/pages/index/index.js
@@ -2,6 +2,13 @@ const app = getApp();
 
 Page({
   data: {
+    custom_active: 0,  // 0.免费打样专区 1.小批量定制 2.免开模费
+
+    page: 1,
+    list: [],
+    nomore: true,
+    nodata: false,
+    loading: false
   },
   onLoad() {
   }
